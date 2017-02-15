74cd81be777eeb72cf44ef6f8aff4807a69968cf
Matthew Thomas <matthew.e.thomas@outlook.com>
2017-02-14 23:27:23 -0600

    adding initial build and test script

---
+++ build.ps1
@@ -0,0 +1,11 @@
+﻿$appDir = "Source\CoreLeaf"
+$testDir = "Source\CoreLeaf.Tests"
+
+dotnet clean $appDir
+dotnet restore $appDir
+dotnet build $appDir
+
+dotnet clean $testDir
+dotnet restore $testDir
+dotnet build $testDir
+dotnet test $testDir\CoreLeaf.Tests.csproj
