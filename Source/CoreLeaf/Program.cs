0a7d2c2513c86bee1baee041c2a2752dab178cf1
Matthew Thomas <matthew.e.thomas@outlook.com>
2017-02-14 21:39:37 -0600

    creating initial commit

---
+++ Source/CoreLeaf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreLeaf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
