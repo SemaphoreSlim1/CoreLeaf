4adaf328c6ad156dc018929713995d705eada1c1
Matthew Thomas <matthew.e.thomas@outlook.com>
2017-03-18 01:05:59 -0500

    adding blowfish encryption

---
+++ Source/CoreLeaf/Encryption/IBlowfish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreLeaf.Encryption
+{
+    public interface IBlowfish
+    {
+        string ApplyPkcs5Padding(string str);
+
+        string Encrypt_ECB(string pt);
+
+        string Decrypt_ECB(string ct);
+    }
+}
